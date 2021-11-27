2c0e2a14044f352baefb5762db8d440beec569e6
AtaberkGozkaya <ataberkgozkaya1071@gmail.com>
2021-11-27 14:11:26 +0300

    Update LogData.js

--- getir-nodejs-bootcamp-week-2-assignment/LogData.js
+++ getir-nodejs-bootcamp-week-2-assignment/LogData.js
@@ -1,4 +1,6 @@
+//import module
 const fs = require('fs');
+//method for writing logs to a file
 module.exports = (file, data) => {
     return new Promise((resolve, reject) => {
         fs.writeFile(file, data, {'flag':'a'}, err => {
